270eab322e607e4065c0617eb36d0bd22b4b98f5
渡邊和貴 <j2241@matsue-ct.ac.jp>
2025-06-26 05:43:35 +0000

    14の途中

---
+++ knexfile.js
@@ -0,0 +1,44 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: "mysql",
+    connection: {
+      database: "todo_app",
+      user: "root",
+      password: "[事前準備で設定したrootユーザのパスワード]",
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+  },
+
+  staging: {
+    client: "mysql",
+    connection: {
+      database: "todo_app",
+      user: "root",
+      password: "[事前準備で設定したrootユーザのパスワード]",
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+  },
+
+  production: {
+    client: "mysql",
+    connection: {
+      database: "todo_app",
+      user: "root",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+  }
+
+};
---
+++ 任意の場所/knex.js
@@ -0,0 +1,6 @@
+const environment = "development";
+const config = require("../knexfile.js")[environment];
+const knex = require("knex")(config);
+const mysql = require('mysql');
+knex("tasks").select("*");
+module.exports = knex;
